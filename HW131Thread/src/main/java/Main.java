e4a44d73199cbf92d8611b304b0e60d14251bedc
Artem <thefirstbornson@gmail.com>
2018-11-06 17:55:56 +0200

    HW 13.1 ver 1.1 Donskogo 1

--- HW131Thread/src/main/java/Main.java
+++ HW131Thread/src/main/java/Main.java
@@ -1,4 +1,4 @@
-import java.util.Arrays;
+import java.util.*;
 
 public class Main {
     int[] a = {2, 3, 1};
@@ -11,21 +11,72 @@ public class Main {
 //        for (int i:both) {
 //            System.out.print(i);
 //        }
-        Main m = new Main();
-        Thread t1 = new Thread(()->{m.sort();});
-        Thread t2 = new Thread(()->{m.sort();});
+       Main m = new Main();
+//        Thread t1 = new Thread(()->{m.sort();});
+//        Thread t2 = new Thread(()->{m.sort();});
+//
+//        t1.start();
+//        t2.start();
+//
+//        t1.join();
+//        t2.join();
+//       for (int i:m.c) {
+//            System.out.print(i);
+//        }
+        int sourceSize =101;
+        int countOfPartitions = 4;
+        int lenOfSubList = sourceSize%countOfPartitions==0
+                           ? sourceSize/countOfPartitions
+                           : sourceSize/countOfPartitions+1;
 
-        t1.start();
-        t2.start();
-
-        t1.join();
-        t2.join();
-       for (int i:m.c) {
-            System.out.print(i);
+        ArrayList source = new ArrayList(sourceSize);
+        Random rand = new Random();
+        rand.setSeed(System.currentTimeMillis());
+        for (int i=0; i<sourceSize; i++)
+        {
+            Integer r = rand.nextInt() % 256;
+            source.add(r);
         }
+       // source.stream().forEach(System.out::print);
+
+       // Collections.sort(source);
+
+        ArrayList<List> subLists = new ArrayList(countOfPartitions);
+        int i=0;
+        int j=lenOfSubList;
+        while (sourceSize>i){
+            if (source.size()-i>lenOfSubList){
+                subLists.add(source.subList(i,j));
+                i += lenOfSubList;
+                j += lenOfSubList;
+            }
+            else {
+                subLists.add(source.subList(i,source.size()));
+                i=source.size();
+            }
+        }
+
+        System.out.println();
+        for (List list: subLists) {
+
+            Thread t = new Thread(()->{Collections.sort(list);});
+            t.start();
+            t.join();
+            list.stream().forEach(System.out::print);
+            System.out.println();
+        }
+        System.out.println();
+
+
     }
 
-    synchronized void sort(){
-        Arrays.sort(c);
+     List merge(ArrayList<List> subLists){
+        List res = new ArrayList(subLists.get(0));
+        for(int i=1;i<subLists.size();i++){
+
+        }
+
+
+       return null;
     }
 }

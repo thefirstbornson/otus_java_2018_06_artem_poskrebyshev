80dab0d16e9eb56ec57d69c3534120b0b190ee44
Artem <thefirstbornson@gmail.com>
2018-11-22 11:23:58 +0200

    HW 15.1 ver 1.1 Maysky per.1 servlet problem

--- HW151MessageSystem/src/main/java/websocket/UsersCntWebSocket.java
+++ HW151MessageSystem/src/main/java/websocket/UsersCntWebSocket.java
@@ -2,6 +2,9 @@ package websocket;
 
 import base.DBService;
 import datasets.UserDataSet;
+import org.eclipse.jetty.websocket.api.Session;
+import org.eclipse.jetty.websocket.api.annotations.OnWebSocketClose;
+import org.eclipse.jetty.websocket.api.annotations.OnWebSocketConnect;
 import org.eclipse.jetty.websocket.api.annotations.WebSocket;
 import servlets.TemplateProcessor;
 
@@ -9,25 +12,22 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.util.Map;
 
 @WebSocket
 public class UsersCntWebSocket {
 
     private static final String GETUSER_PAGE_TEMPLATE = "numusers.html";
 
-    private final TemplateProcessor templateProcessor;
-    private final DBService dbService;
+    private  TemplateProcessor templateProcessor;
+    private  DBService dbService;
 
-    public UsersCntWebSocket(TemplateProcessor templateProcessor, DBService dbService) {
-        this.templateProcessor = templateProcessor;
-        this.dbService = dbService;
-    }
+//    public UsersCntWebSocket(TemplateProcessor templateProcessor, DBService dbService) {
+//    }
 
-    public void doGet(HttpServletRequest request,
-                      HttpServletResponse response) throws ServletException, IOException {
-        doPost(request, response);
-    }
+//    public void doGet(HttpServletRequest request,
+//                      HttpServletResponse response) throws ServletException, IOException {
+//        doPost(request, response);
+//    }
 
     public void doPost(HttpServletRequest request,
                        HttpServletResponse response) throws ServletException, IOException {
@@ -35,10 +35,23 @@ public class UsersCntWebSocket {
         Integer usersNum = dbService.readAll(UserDataSet.class).size();
         String value = usersNum!=null?usersNum.toString():"not found";
 
-        String page = templateProcessor.getPage(GETUSER_PAGE_TEMPLATE, Map.of("userNum", value));//save to the page
+        //String page = templateProcessor.getPage(GETUSER_PAGE_TEMPLATE, Map.of("userNum", value));//save to the page
 
-        response.setContentType("text/html;charset=utf-8");
-        response.setStatus(HttpServletResponse.SC_OK);
-        response.getWriter().println(page);
+//        response.setContentType("text/html;charset=utf-8");
+//        response.setStatus(HttpServletResponse.SC_OK);
+//        response.getWriter().println(page);
     }
+
+    @OnWebSocketConnect
+    public void onConnect(Session session) throws IOException {
+        System.out.println(session.getRemoteAddress().getHostString() + " connected!");
+        this.templateProcessor = templateProcessor;
+        this.dbService = dbService;
+    }
+
+    @OnWebSocketClose
+    public void onClose(Session session, int status, String reason) {
+        System.out.println(session.getRemoteAddress().getHostString() + " closed!");
+    }
+
 }
